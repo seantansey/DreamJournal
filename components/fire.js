f1c4c0126478d8d4158ff0634ed1b887ad889307
Sean Tansey <sean.d.tansey@gmail.com>
2019-06-17 19:14:12 -0600

    firebase configured minus the project id issue in .env

--- components/fire.js
+++ components/fire.js
@@ -5,7 +5,7 @@ import firebase from 'firebase'
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: 'dreamjournal-c7c23',
+    projectId: "dreamjournal-c7c23",
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
