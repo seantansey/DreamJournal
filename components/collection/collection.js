6ac13b8f31dcc6d86f3f460a865ec55576150d9b
Sean Tansey <sean.d.tansey@gmail.com>
2019-06-18 13:16:08 -0600

    fixed useEffect issue, dreamlist renders, can add new dreams

--- components/collection/collection.js
+++ components/collection/collection.js
@@ -1,28 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Header, ListItem } from 'react-native-elements'
 import firebase from 'firebase'
 import 'firebase/firestore'
 
 const Collection = () => {
-  const count = 0
-  const retrieveCollection = (userId) => {
-    firebase.firestore().collection('Dreams')
-      .where('user', '==', userId)
-      // does this even need to be real time?
-      .onSnapshot(snapshot => {
-        let newDreams = snapshot.docChanges()
-        newDreams.forEach(doc => {
-          console.log(doc.doc.data())
-        })
-      })
-  }
+  const [ dreams, setDreams ] = useState([])
 
   useEffect(() => {
+    const retrieveCollection = (userId) => {
+      firebase.firestore().collection('Dreams')
+        .where('user', '==', userId)
+        .onSnapshot(snapshot => {
+          let newDreams = snapshot.docChanges()
+          newDreams.forEach(doc => {
+            setDreams(prevDreams => [...prevDreams, doc.doc.data()])
+          })
+        })
+    }
     retrieveCollection('N25c9lKITZQ7JtPEZSrMX6uC7Ot2')
-  }, [count])
+  }, [])
+
+
+  const renderDreams = () => {
+    return dreams.map((info, i) =>
+      <ListItem
+        key={i}
+        title={info.title}
+        style={styles.list}
+        chevron
+      />
+    )
+  }
 
   return (
-    null
+    <View>
+      <Header
+        centerComponent={{
+          text: 'Collection',
+          style: { color: '#fff', fontSize: 22 } }}
+      />
+      {renderDreams()}
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  list: {
+    borderWidth: .5,
+    borderColor: "grey",
+  }
+})
 
 export default Collection
